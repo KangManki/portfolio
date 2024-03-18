751e28eb1eb4a054d882b8ca67146b65e6d9d992
KangManki <constant1022@gmail.com>
2024-03-18 17:42:53 +0900

    JS - Home

--- src/main.js
+++ src/main.js
@@ -23,13 +23,35 @@
 
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
-document.addEventListener('scroll', () =>  {
-console.log(window.scrollY);
+const headerHeight = header.offsetHeight;
 
+
+document.addEventListener('scroll', () =>  {
 if(window.scrollY > headerHeight) {
   header.classList.add('header--bgColor--dark');
 } else  {
   header.classList.remove('header--bgColor--dark');
 }
+
+
 });
+
+
+
+//Home Section Page 스크롤시 투평하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () =>  {
+  
+  // 내가 작성한 코드
+  let ratio = (homeHeight - window.scrollY) / homeHeight;  
+
+  // 드림코딩 코드
+  let ratio_dream = 1 -  (window.scrollY / homeHeight);  
+
+  // 값은 동일하다.
+
+  console.log(`내가 작성한 ratio - ${ratio} 드림코딩 ratio - ${ratio_dream}`);
+  home.style.opacity = ratio < 0 ? 0 : ratio;  
+  
+  });
