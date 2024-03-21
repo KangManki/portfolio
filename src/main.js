8adaa724b220c930b250fec8e9a53f067405a4ef
KangManki <constant1022@gmail.com>
2024-03-21 13:30:07 +0900

    JS-projects

--- src/main.js
+++ src/main.js
@@ -1,25 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function()  {
-//  setEvent();
-// });
-
-
-// function setEvent() {
-//   window.addEventListener("scroll", function()  {
-//     // let header = document.querySelector(".header");
-
-//     let header = document.querySelector(".header");
-    
-//     let hHeight = header.offsetHeight;
-
-    
-
-//     hHeight > this.window.scrollY ? header.classList.add("header--bgColor") : header.classList.remove("header--bgColor");
-
-
-//   });
-// }
-
-
+'use strict';
 
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
