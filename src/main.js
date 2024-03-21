96eb41f6bd2a7a6bb04ca7eeed516dfcd83f6335
KangManki <constant1022@gmail.com>
2024-03-21 13:24:22 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -64,3 +64,18 @@ document.addEventListener('scroll', () =>  {
 
   });
   
+
+
+  // Navbar 토글버튼 클릭 처리
+  const navbarMenu = document.querySelector('.header__menu');
+  const navbarToggle = document.querySelector('.header__toggle');
+
+  navbarToggle.addEventListener('click', () =>  {
+    navbarMenu.classList.toggle('open');
+  });
+
+
+  // Navbar 메뉴 클릭시 자동으로 닫아줌
+  navbarMenu.addEventListener('click', () =>  {
+    navbarMenu.classList.remove('open');
+  });
